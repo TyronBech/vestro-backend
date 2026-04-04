bd3cebcd1b36fedbf609430f71fb3b1dc335764b
TyronBech <tyronbechayda1112@gmail.com>
2026-04-04 21:01:49 +0800

    refactor: remove HOST variable and update server log message to use localhost

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
 import { createApp } from "./app";
-import { HOST, PORT } from "./config/env";
+import { PORT } from "./config/env";
 
 const app = createApp();
 
-app.listen(PORT, HOST, () => {
-  console.log(`Vestro backend running at http://${HOST}:${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Vestro backend running at http://localhost:${PORT}`);
 });
